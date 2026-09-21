1b8b574c7021701fdb8c2a432027bf76b7df383c
duvanbo12 <duvan71213@gmail.com>
2026-09-21 16:56:18 -0500

    Corregir clase Producto y nombre de archivo

--- app/Models/producto.php
+++ app/Models/producto.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class producto extends Model
+class Producto extends Model
 {
-    //
+    protected $table = 'productos'; // Asegura que apunte a la tabla correcta
 }
