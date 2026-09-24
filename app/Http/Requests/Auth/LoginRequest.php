c1576cad2b9a217d57b4336e1bad68f2d1c658eb
duvanbo12 <duvan71213@gmail.com>
2026-09-23 22:20:12 -0500

    ultimo cambio

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -26,40 +26,40 @@ class LoginRequest extends FormRequest
 
     public function authenticate(): void
     {
-        \->ensureIsNotRateLimited();
+        $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt(\->only('email', 'password'), \->boolean('remember'))) {
-            RateLimiter::hit(\->throttleKey());
+        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+            RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
                 'email' => __('Estas credenciales no coinciden con nuestros registros.'),
             ]);
         }
 
-        RateLimiter::clear(\->throttleKey());
+        RateLimiter::clear($this->throttleKey());
     }
 
     public function ensureIsNotRateLimited(): void
     {
-        if (! RateLimiter::tooManyAttempts(\->throttleKey(), 5)) {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
             return;
         }
 
-        event(new Lockout(\));
+        event(new Lockout($this));
 
-        \ = RateLimiter::availableIn(\->throttleKey());
+        $seconds = RateLimiter::availableIn($this->throttleKey());
 
         throw ValidationException::withMessages([
             'email' => trans('auth.throttle', [
-                'seconds' => \,
-                'minutes' => ceil(\ / 60),
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
             ]),
         ]);
     }
 
     public function throttleKey(): string
     {
-        return Str::transliterate(Str::lower(\->input('email')).'|'.\->ip());
+        return Str::transliterate(Str::lower($this->input('email')) . '|' . $this->ip());
     }
 }
 
